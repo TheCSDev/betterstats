85a67c04fd128f1d4dfbd9b80cbe4e0c5d7c9cc8
TheCSDev <thecsdev@gmail.com>
2026-03-27 22:26:47 +0100

    Release 5.2.0+fn-26.1 (#31)
    This mod can now run on both Fabric and NeoForge simultaneously.

---
+++ src/main/java/com/thecsdev/betterstats/neoforge/server/BetterStatsNeoServer.java
@@ -0,0 +1,14 @@
+package com.thecsdev.betterstats.neoforge.server;
+
+import com.thecsdev.betterstats.server.BetterStatsServer;
+
+/**
+ * The main "dedicated server" entry-point for this mod, that is executed
+ * by the NeoForge loader.
+ */
+public final class BetterStatsNeoServer extends BetterStatsServer
+{
+	// ==================================================
+	public BetterStatsNeoServer() {}
+	// ==================================================
+}
