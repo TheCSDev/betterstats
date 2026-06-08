ec1656b96940db31d20ef8d03b094029757cb45d
TheCSDev <thecsdev@gmail.com>
2026-06-08 17:56:40 +0200

    Update AbstractMcbsGoalEditScreen.java
    Goal editing preview GUI skeleton

--- src/main/java/com/thecsdev/betterstats/api/client/gui/screen/AbstractMcbsGoalEditScreen.java
+++ src/main/java/com/thecsdev/betterstats/api/client/gui/screen/AbstractMcbsGoalEditScreen.java
@@ -2,16 +2,22 @@ package com.thecsdev.betterstats.api.client.gui.screen;
 
 import com.thecsdev.betterstats.api.mcbs.controller.tab.McbsEditorFileTab;
 import com.thecsdev.betterstats.api.mcbs.model.goal.McbsGoal;
+import com.thecsdev.betterstats.api.mcbs.view.goal.McbsGoalGUI;
 import com.thecsdev.betterstats.api.mcbs.view.statsview.StatsViewUtils;
+import com.thecsdev.betterstats.client.gui.widget.BGoalProgressBar;
 import com.thecsdev.betterstats.resource.BLanguage;
 import com.thecsdev.common.math.UDim2;
 import com.thecsdev.common.util.enumerations.CompassDirection;
 import com.thecsdev.commonmc.api.client.gui.TElement;
+import com.thecsdev.commonmc.api.client.gui.label.TLabelElement;
 import com.thecsdev.commonmc.api.client.gui.misc.TFillColorElement;
+import com.thecsdev.commonmc.api.client.gui.misc.TSeparatorElement;
 import com.thecsdev.commonmc.api.client.gui.panel.TPanelElement;
 import com.thecsdev.commonmc.api.client.gui.screen.ILastScreenProvider;
 import com.thecsdev.commonmc.api.client.gui.screen.TScreen;
 import com.thecsdev.commonmc.api.client.gui.screen.TScreenPlus;
+import com.thecsdev.commonmc.api.client.gui.tooltip.TTooltip;
+import com.thecsdev.commonmc.api.client.gui.widget.TButtonWidget;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.screens.Screen;
@@ -24,6 +30,8 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Objects;
+
+import static com.thecsdev.betterstats.api.mcbs.view.statsview.StatsViewUtils.GAP;
 
 /**
  * Abstract {@link TScreen} implementation that provides a template for
@@ -42,6 +50,7 @@ public abstract class AbstractMcbsGoalEditScreen<G extends McbsGoal>
 	private final @NotNull  Identifier        goalId;
 	private       @NotNull  G                 goal;
 	private       @NotNull  G                 preview;
+	private final @Nullable McbsGoalGUI<G>    goalGui;
 	// ==================================================
 	/**
 	 * Constructs an instance of this screen.
@@ -71,6 +80,7 @@ public abstract class AbstractMcbsGoalEditScreen<G extends McbsGoal>
 				.orElseThrow(() -> new NoSuchElementException("Editor tab doesn't contain the given goal"));
 		this.goal        = Objects.requireNonNull(goal);
 		this.preview     = (G) goal.clone();
+		this.goalGui     = McbsGoalGUI.findFor(goal);
 	}
 	// ==================================================
 	/**
@@ -119,25 +129,6 @@ public abstract class AbstractMcbsGoalEditScreen<G extends McbsGoal>
 	}
 	// ==================================================
 	/**
-	 * Initializes GUI interface for previewing the current live state of
-	 * {@link #getPreviewGoal()}.
-	 * @param body The {@link TElement} where the preview interface is to be placed.
-	 */
-	@ApiStatus.Internal
-	private final void initGoalPreviewGui(@NotNull TElement body)
-	{
-		//initialize scrollable panel element
-		final var el_panel = new TPanelElement.Paintable(0, 0, 0x33FFFFFF);
-		el_panel.setBounds(body.getBounds());
-		el_panel.scrollPaddingProperty().set(7, AbstractMcbsGoalEditScreen.class);
-		body.add(el_panel);
-
-		//FIXME - implement the interface
-		final var lbl_group = StatsViewUtils.initGroupLabel(el_panel, Component.literal("Preview"));
-		lbl_group.textAlignmentProperty().set(CompassDirection.CENTER, AbstractMcbsGoalEditScreen.class);
-	}
-	// --------------------------------------------------
-	/**
 	 * Initializes GUI interface for editing the {@link #getPreviewGoal()},
 	 * to a given {@link TElement} that acts as the baseline "body".
 	 * @param body The {@link TElement} where the editing iterface is to be placed.
@@ -171,10 +162,20 @@ public abstract class AbstractMcbsGoalEditScreen<G extends McbsGoal>
 	private final class Interface extends TFillColorElement
 	{
 		// ==================================================
+		private final @NotNull  McbsEditorFileTab editorTab = AbstractMcbsGoalEditScreen.this.editorTab;
+		// --------------------------------------------------
+		private       @NotNull  G                 goal      = AbstractMcbsGoalEditScreen.this.goal;
+		private       @NotNull  G                 preview   = AbstractMcbsGoalEditScreen.this.preview;
+		private final @Nullable McbsGoalGUI<G>    goalGui   = AbstractMcbsGoalEditScreen.this.goalGui;
+		// ==================================================
 		public Interface() { super(0x3B000000, 0xFF000000); }
 		// ==================================================
 		protected final @Override void initCallback()
 		{
+			//refresh field values
+			this.goal    = AbstractMcbsGoalEditScreen.this.goal;
+			this.preview = AbstractMcbsGoalEditScreen.this.preview;
+
 			//initialize baseline bodies for the gui
 			final var el_left = new TFillColorElement(0x3B000000, 0);
 			add(el_left);
@@ -185,8 +186,109 @@ public abstract class AbstractMcbsGoalEditScreen<G extends McbsGoal>
 			el_right.setBounds(new UDim2(0.35, 0, 0, 1), new UDim2(0.65, -1, 1, -2));
 
 			//initialize preview and editing interfaces
-			AbstractMcbsGoalEditScreen.this.initGoalPreviewGui(el_left);
+			initGoalPreviewGui(el_left);
 			AbstractMcbsGoalEditScreen.this.initGoalEditorGui(el_right);
+		}
+		// ==================================================
+		/**
+		 * Initializes GUI interface for previewing the relevant {@link McbsGoal}s.
+		 * @param body The {@link TElement} where the preview interface is to be placed.
+		 * @throws NullPointerException If the argument is {@code null}.
+		 */
+		@ApiStatus.Internal
+		private final void initGoalPreviewGui(@NotNull TElement body)
+				throws NullPointerException
+		{
+			//initialize scrollable panel element
+			final var el_panel = new TPanelElement.Paintable(0, 0, 0x33FFFFFF);
+			el_panel.setBounds(body.getBounds());
+			el_panel.scrollPaddingProperty().set(7, Interface.class);
+			body.add(el_panel);
+
+			//group label that says "Preview"
+			final var lbl_group = StatsViewUtils.initGroupLabel(el_panel, Component.literal("Preview"));
+			lbl_group.textAlignmentProperty().set(CompassDirection.CENTER, Interface.class);
+
+			//preview for "From:"
+			final var lbl_from = StatsViewUtils.initGroupLabel(el_panel, Component.literal("From:"));
+			lbl_from.textColorProperty().set(0xFFC6C6C6, Interface.class);
+			initGoalPreviewGui(el_panel, this.goal);
+
+			//preview for "To:"
+			final var lbl_to = StatsViewUtils.initGroupLabel(el_panel, Component.literal("To:"));
+			lbl_to.textColorProperty().set(0xFFC6C6C6, Interface.class);
+			initGoalPreviewGui(el_panel, this.preview);
+
+			//separator for buttons below
+			final var el_separator = new TSeparatorElement();
+			el_separator.setBounds(el_panel.computeNextYBounds(15, GAP).add(10, 0, -20, 0));
+			el_panel.add(el_separator);
+
+			//holder element that holds action buttons
+			final var el_btns = new TElement();
+			el_btns.setBounds(el_panel.computeNextYBounds(20, GAP));
+			el_panel.add(el_btns);
+
+			//submit button
+			final var btn_submit = new TButtonWidget();
+			btn_submit.setBounds(el_btns.getBounds().endX - 35, el_btns.getBounds().y, 35, 20);
+			btn_submit.getLabel().setText(Component.literal("✔"));
+			btn_submit.eClicked.addListener(_ -> {
+				AbstractMcbsGoalEditScreen.this.applyChanges();
+				AbstractMcbsGoalEditScreen.this.close();
+			});
+			el_btns.add(btn_submit);
+
+			//cancel button
+			final var btn_cancel = new TButtonWidget();
+			btn_cancel.setBounds(el_btns.getBounds().endX - 73, el_btns.getBounds().y, 35, 20);
+			btn_cancel.getLabel().setText(Component.literal("✖"));
+			btn_cancel.eClicked.addListener(_ -> AbstractMcbsGoalEditScreen.this.close());
+			el_btns.add(btn_cancel);
+		}
+
+		/**
+		 * Initializes GUI interface for previewing a specific {@link McbsGoal}.
+		 * @param panel The {@link TPanelElement} where the GUI interface goes.
+		 * @param goal The {@link McbsGoal} to preview.
+		 * @throws NullPointerException If an argument is {@code null}.
+		 */
+		@ApiStatus.Internal
+		private final void initGoalPreviewGui(@NotNull TPanelElement panel, @NotNull G goal)
+				throws NullPointerException
+		{
+			//initialize base element for holding icon and objective text
+			final var el1 = new TElement();
+			el1.setBounds(panel.computeNextYBounds(20, GAP));
+			panel.add(el1);
+
+			//initialize goal icon
+			final var el_ico = new TFillColorElement(0x3B000000, 0xFF000000);
+			el_ico.setBounds(el1.getBounds().x, el1.getBounds().y, 20, 20);
+			el1.add(el_ico);
+
+			if(this.goalGui != null)
+				this.goalGui.initIcon(goal, el_ico, el_ico.getBounds().height / 10);
+
+			//initialize goal objective label
+			final var lbl_objective = new TLabelElement(goal.getObjectiveText());
+			lbl_objective.setBounds(el1.getBounds().add(25, 0, -25, -10));
+			lbl_objective.tooltipProperty().set(_ -> TTooltip.of(lbl_objective.getText()), Interface.class);
+			lbl_objective.hoverableProperty().set(true, Interface.class);
+			el1.add(lbl_objective);
+
+			//initialize goal progress label
+			final var lbl_progress = new TLabelElement(goal.getProgressText(this.editorTab.getMcbsFile()));
+			lbl_progress.setBounds(el1.getBounds().add(25, 10, -25, -10));
+			lbl_progress.textScaleProperty().set(0.9, Interface.class);
+			lbl_progress.textColorProperty().set(0xFFC6C6C6, Interface.class);
+			el1.add(lbl_progress);
+
+			//initialize goal progress bar
+			final var el_progress = new BGoalProgressBar();
+			el_progress.setBounds(panel.computeNextYBounds(12, GAP));
+			el_progress.setValue(goal.getProgress(this.editorTab.getMcbsFile()));
+			panel.add(el_progress);
 		}
 		// ==================================================
 	}
